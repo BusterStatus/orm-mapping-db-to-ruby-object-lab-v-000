870d742eeba07549b8be7f69131836734de51186
matthew.serre <matthew.serre@gmail.com>
2019-03-13 18:59:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,11 +79,11 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10
+      WHERE grade = ?
       LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, 10, x).map do |row|
+    DB[:conn].execute(sql, 10).map do |row|
       self.new_from_db(row)
     end.first
   end
