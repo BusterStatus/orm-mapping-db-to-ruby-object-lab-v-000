72aa8f7fe0bd3a095515793690077b8b129dc060
matthew.serre <matthew.serre@gmail.com>
2019-03-13 18:55:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,14 +38,14 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_9 (grade)
+  def self.all_students_in_grade_9
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = ?
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, 9).map do |row|
       self.new_from_db(row)
     end
   end
